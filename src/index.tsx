02e8a15183f3eb09c46d7a1b59838045d7881216
Charlie Gleason <hi@charliegleason.com>
2021-11-26 17:34:54 +0000

    First pass at a functioning extension

--- src/index.tsx
+++ src/index.tsx
@@ -1,66 +1,83 @@
-import { ActionPanel, CopyToClipboardAction, List, OpenInBrowserAction, showToast, ToastStyle } from "@raycast/api";
+import { ActionPanel, CopyToClipboardAction, List, OpenInBrowserAction, showToast, ToastStyle, environment } from "@raycast/api";
 import { useState, useEffect } from "react";
 import fetch from "node-fetch";
+import fs from "fs";
+import path from "path";
 
-export default function ArticleList() {
-  const [state, setState] = useState<{ articles: Article[] }>({ articles: [] });
+export default function GitIgnoreList() {
+  const [state, setState] = useState<{ files: GitIgnore[] }>({ files: [] });
 
   useEffect(() => {
     async function fetch() {
-      const articles = await fetchArticles();
+      const files = await fetchFiles();
       setState((oldState) => ({
         ...oldState,
-        articles: articles,
+        files: files
+          .filter(file => path.parse(file.path).ext === ".gitignore")
       }));
     }
     fetch();
   }, []);
 
   return (
-    <List isLoading={state.articles.length === 0} searchBarPlaceholder="Filter articles by name...">
-      {state.articles.map((article) => (
-        <ArticleListItem key={article.id} article={article} />
+    <List isLoading={state.files.length === 0} searchBarPlaceholder="Filter files by name...">
+      <List.Item
+        id="link"
+        key="link"
+        title="View Gitignore repository"
+        icon="list-icon.png"
+        actions={
+          <ActionPanel>
+            <OpenInBrowserAction url="https://github.com/github/gitignore" />
+          </ActionPanel>
+        }
+      />
+      
+      {state.files.map((gitignore) => (
+        <GitIgnoreListItem key={gitignore.sha} gitignore={gitignore} />
       ))}
     </List>
   );
 }
 
-function ArticleListItem(props: { article: Article }) {
-  const article = props.article;
-
+function GitIgnoreListItem(props: { gitignore: GitIgnore }) {
+  const gitignore = props.gitignore;
+  const contentPath = `${environment.assetsPath}/gitignore/${gitignore.path}`;
+  const contentExists = fs.existsSync(contentPath)
   return (
     <List.Item
-      id={article.id}
-      key={article.id}
-      title={article.title}
-      subtitle="Raycast Blog"
+      id={gitignore.sha}
+      key={gitignore.sha}
+      title={gitignore.name.replace('.gitignore','')}
       icon="list-icon.png"
-      accessoryTitle={new Date(article.date_modified).toLocaleDateString()}
+      accessoryTitle={gitignore.name}
+      subtitle={contentExists ? "" : "Open on GitHub.com"}
       actions={
         <ActionPanel>
-          <OpenInBrowserAction url={article.url} />
-          <CopyToClipboardAction title="Copy URL" content={article.url} />
+          {contentExists && <CopyToClipboardAction content={fs.readFileSync(`${environment.assetsPath}/gitignore/${gitignore.path}`, 'utf8')} />}
+          <OpenInBrowserAction url={gitignore.download_url} />
         </ActionPanel>
       }
     />
   );
 }
 
-async function fetchArticles(): Promise<Article[]> {
+async function fetchFiles(): Promise<GitIgnore[]> {
   try {
-    const response = await fetch("https://raycast.com/feed.json");
+    const response = await fetch("https://api.github.com/repos/github/gitignore/contents/");
     const json = await response.json();
-    return (json as Record<string, unknown>).items as Article[];
+    return (json as Record<string, unknown>) as unknown as GitIgnore[];
   } catch (error) {
     console.error(error);
-    showToast(ToastStyle.Failure, "Could not load articles");
+    showToast(ToastStyle.Failure, "Could not load gitignore files");
     return Promise.resolve([]);
   }
 }
 
-type Article = {
-  id: string;
-  title: string;
-  url: string;
-  date_modified: string;
+type GitIgnore = {
+  sha: string;
+  name: string;
+  path: string;
+  download_url: string;
+  type: string;
 };
