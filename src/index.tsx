b38ad69984b2bbcdc1ae2643865b40d445797898
Charlie Gleason <hi@charliegleason.com>
2021-11-29 19:19:25 +0000

    Updated script, tidied constants

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
-import { ActionPanel, CopyToClipboardAction, Icon, List, OpenInBrowserAction, showToast, ToastStyle, environment } from "@raycast/api";
+import {
+  ActionPanel,
+  CopyToClipboardAction,
+  Icon,
+  List,
+  OpenInBrowserAction,
+  showToast,
+  ToastStyle,
+  environment,
+} from "@raycast/api";
 import { useState, useEffect } from "react";
-import fetch from "node-fetch";
+import fetch from "cross-fetch";
 import fs from "fs";
 import path from "path";
 
+const API_URL = "https://gitignore-fetch.superhighfives.workers.dev";
 const GITHUB_URL = "https://github.com/github/gitignore";
-const FETCH_URL = "https://gitignore-fetch.superhighfives.workers.dev";
 
 export default function GitIgnoreList() {
   const [state, setState] = useState<{ files: GitIgnore[] }>({ files: [] });
@@ -15,8 +24,7 @@ export default function GitIgnoreList() {
       const files = await fetchFiles();
       setState((oldState) => ({
         ...oldState,
-        files: files
-          .filter(file => path.parse(file.path).ext === ".gitignore")
+        files: files.filter((file) => path.parse(file.path).ext === ".gitignore"),
       }));
     }
     fetch();
@@ -35,7 +43,7 @@ export default function GitIgnoreList() {
           </ActionPanel>
         }
       />
-      
+
       {state.files.map((gitignore) => (
         <GitIgnoreListItem key={gitignore.sha} gitignore={gitignore} />
       ))}
@@ -46,18 +54,22 @@ export default function GitIgnoreList() {
 function GitIgnoreListItem(props: { gitignore: GitIgnore }) {
   const gitignore = props.gitignore;
   const contentPath = `${environment.assetsPath}/gitignore/${gitignore.path}`;
-  const contentExists = fs.existsSync(contentPath)
+  const contentExists = fs.existsSync(contentPath);
   return (
     <List.Item
       id={gitignore.sha}
       key={gitignore.sha}
-      title={gitignore.name.replace('.gitignore','')}
+      title={gitignore.name.replace(".gitignore", "")}
       icon={contentExists ? Icon.Document : Icon.Link}
       accessoryTitle={gitignore.name}
       subtitle={contentExists ? "" : "Open on GitHub.com"}
       actions={
         <ActionPanel>
-          {contentExists && <CopyToClipboardAction content={fs.readFileSync(`${environment.assetsPath}/gitignore/${gitignore.path}`, 'utf8')} />}
+          {contentExists && (
+            <CopyToClipboardAction
+              content={fs.readFileSync(`${environment.assetsPath}/gitignore/${gitignore.path}`, "utf8")}
+            />
+          )}
           <OpenInBrowserAction url={gitignore.download_url} />
         </ActionPanel>
       }
@@ -67,9 +79,9 @@ function GitIgnoreListItem(props: { gitignore: GitIgnore }) {
 
 async function fetchFiles(): Promise<GitIgnore[]> {
   try {
-    const response = await fetch(FETCH_URL);
+    const response = await fetch(API_URL);
     const json = await response.json();
-    return (json as Record<string, unknown>) as unknown as GitIgnore[];
+    return json as Record<string, unknown> as unknown as GitIgnore[];
   } catch (error) {
     console.error(error);
     showToast(ToastStyle.Failure, "Could not load gitignore files");
